6e841d07c2a41431d179dfa14499771cc49e9896
sayray <sayray@paypal.com>
2021-05-25 20:34:34 +0530

    Initial commit

---
+++ src/main/java/com/myexample/spring5jokesapp/Spring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.myexample.spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppApplication.class, args);
+	}
+
+}
